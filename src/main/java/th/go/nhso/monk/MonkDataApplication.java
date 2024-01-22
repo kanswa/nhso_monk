f372e9d14de45362b4b545ef186088b9bb4213fe
kan.sw <kan.sw@g-able.com>
2024-01-22 18:27:45 +0700

    initial commit

---
+++ src/main/java/th/go/nhso/monk/MonkDataApplication.java
@@ -0,0 +1,13 @@
+package th.go.nhso.monk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/go/nhso/monk/MonkDataApplicationTests.java
@@ -0,0 +1,13 @@
+package th.go.nhso.monk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
